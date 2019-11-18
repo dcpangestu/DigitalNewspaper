caa58f1742b64e36222815b1ff2eae11693953bd
Dwi Cahyo Pangestu <dcpangestu@mail.com>
2019-11-18 21:02:21 +0700

    setup API cuaca

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    weather = requests.get('https://openweathermap.org/data/2.5/weather?q=London&appid=2d57d53ccc1e413e485d654921fd72e5')
+    return weather.text
+    #return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
