2c98286c7bbf5527a05adccaca876ef66c546122
alfinm01 <arrestral@gmail.com>
2019-11-25 23:24:12 +0700

    Add news api getter

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ def home():
 
 @app.route('/weather/<region>')
 def weather(region):
-	weather = requests.get('https://openweathermap.org/data/2.5/weather?q=' +  +  '&appid=' + os.getenv("WEATHER_TOKEN"))
+	weather = requests.get('https://openweathermap.org/data/2.5/weather?q=' + region + '&appid=' + os.getenv("WEATHER_TOKEN"))
     return weather.text
+
+@app.route('/news/<region>')
+def news(region):
+	news = requests.get('https://newsapi.org/v2/everything?q=' + region + '&apiKey=' + os.getenv("NEWS_TOKEN"))
+    return news.text
 
 if __name__ == '__main__':
     app.run(port = os.getenv("PORT"), debug = True)
