a1f982f40091864abdec74ebe4042fc98c5df003
alfinm01 <arrestral@gmail.com>
2019-11-25 23:17:55 +0700

    Refactor weather

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    weather = requests.get('https://openweathermap.org/data/2.5/weather?q=London&appid=2d57d53ccc1e413e485d654921fd72e5')
+    return render_template("home.html")
+
+@app.route('/weather/<region>')
+def weather(region):
+	weather = requests.get('https://openweathermap.org/data/2.5/weather?q=' +  +  '&appid=' + os.getenv("WEATHER_TOKEN"))
     return weather.text
-    #return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(port = os.getenv("PORT"), debug = True)
