6fb5e90ba39498b828932f65dd508413df858bcc
alfinm01 <arrestral@gmail.com>
2019-11-25 23:31:33 +0700

    Change .env to config.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests
+import config
 
 app = Flask(__name__)
 
@@ -15,13 +16,13 @@ def facts(region):
 
 @app.route('/weather/<region>')
 def weather(region):
-	weather = requests.get('https://openweathermap.org/data/2.5/weather?q=' + region + '&appid=' + os.getenv("WEATHER_TOKEN"))
+	weather = requests.get('https://openweathermap.org/data/2.5/weather?q=' + region + '&appid=' + config.WEATHER_TOKEN)
     return weather.text
 
 @app.route('/news/<region>')
 def news(region):
-	news = requests.get('https://newsapi.org/v2/everything?q=' + region + '&apiKey=' + os.getenv("NEWS_TOKEN"))
+	news = requests.get('https://newsapi.org/v2/everything?q=' + region + '&apiKey=' + config.NEWS_TOKEN)
     return news.text
 
 if __name__ == '__main__':
-    app.run(port = os.getenv("PORT"), debug = True)
+    app.run(port = config.PORT, debug = True)
---
+++ sample-config.py
@@ -0,0 +1,3 @@
+PORT = "your_port"
+WEATHER_TOKEN = "your_weathermap_token"
+NEWS_TOKEN = "your_newsapi_token"
