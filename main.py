eb42ea932cff6197b950b8541ef6d10d7ca3ff97
Dwi Cahyo Pangestu <dcpangestu@mail.com>
2019-11-26 14:15:22 +0700

    buat HTML untuk template, home, weather

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import config
 
 app = Flask(__name__)
 
+user = {
+	'name': 'Maulana',
+	'gatau': 'idk dude'
+}
+
 @app.route('/')
 @app.route('/home')
 def index():
 	try:
-		return render_template('home.html', message = 'Digital Newspaper')
+		return render_template('home.html', title = 'Home', user = user)
 	except Exception as e:
 		raise e
 
@@ -23,8 +28,8 @@ def facts(region):
 @app.route('/weather/<region>')
 def weather(region):
 	try:
-		weather = requests.get('https://api.openweathermap.org/data/2.5/weather?q=' + region + '&mode=html&appid=' + config.WEATHER_TOKEN)
-		return render_template('weather.html', data = weather)
+		weather = requests.get('https://api.openweathermap.org/data/2.5/weather?q=' + region + '&appid=' + config.WEATHER_TOKEN)
+		return render_template('weather.html', title = 'Weather: ' + region , region = region, data = weather.text, user = user)
 	except Exception as e:
 		raise e
 
