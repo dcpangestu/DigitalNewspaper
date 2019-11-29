ee602a390572d84b8af94ca0875fe48cfb686625
Dwi Cahyo Pangestu <dcpangestu@mail.com>
2019-11-29 17:28:03 +0700

    ganti route jadi 2 aja, home & <region>

--- main.py
+++ main.py
@@ -11,33 +11,19 @@ user = {
 
 @app.route('/')
 @app.route('/home')
-def index():
+def home():
 	try:
 		return render_template('home.html', title = 'Home', user = user)
 	except Exception as e:
 		raise e
 
-@app.route('/facts/<region>')
-def facts(region):
-	try:
-		facts = requests.get('http://localhost:5000/region/' + region)
-		return render_template('facts.html', title = 'Facts: ' + region , region = region, data = facts.text, user = user)
-	except Exception as e:
-		raise e
-
-@app.route('/weather/<region>')
-def weather(region):
+@app.route('/<region>')
+def kota(region):
 	try:
 		weather = requests.get('https://api.openweathermap.org/data/2.5/weather?q=' + region + '&appid=' + config.WEATHER_TOKEN)
-		return render_template('weather.html', title = 'Weather: ' + region , region = region, data = weather.text, user = user)
-	except Exception as e:
-		raise e
-
-@app.route('/news/<region>')
-def news(region):
-	try:
 		news = requests.get('https://newsapi.org/v2/everything?q=' + region + '&apiKey=' + config.NEWS_TOKEN)
-		return render_template('news.html', title = 'News: ' + region , region = region, data = news.text, user = user)
+		facts = requests.get('http://localhost:5000/region/' + region)
+		return render_template('home.html', title = 'Home', weather = weather, news = news, facts = facts, user = user)
 	except Exception as e:
 		raise e
 
